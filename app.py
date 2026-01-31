4b9ea8ec1310fccea7c3a19dd41103e65b4a0484
Tarika Handoo <156025799+Tarikaily@users.noreply.github.com>
2026-02-01 02:47:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown("Detects suspicious blockchain blocks using percentile-based analysi
 # =========================
 # STEP 1: LOAD DATA
 # =========================
-df = pd.read_csv("dataa/dataset.csv")
+df = pd.read_csv("dataset.csv")
 df.columns = df.columns.str.strip()
 
 st.success("✅ Data Loaded Successfully")
